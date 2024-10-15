8f1421ceb277bcb815a0a38ddb517c7879a7c247
Jose Erick Carreon <support@joserick.com>
2024-10-15 04:19:52 -0600

    feat: Added feature-test

---
+++ tests/Feature/MakeLivewireDiscoverCommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\File;
+use Joserick\LaravelLivewireDiscover\LaravelLivewireDiscover;
+
+it('executes livewire-discover:make command', function () {
+    File::shouldReceive('put')->andReturn(true);
+    File::shouldReceive('exists')->andReturn(false);
+    File::shouldReceive('isDirectory')->andReturn(true);
+
+    LaravelLivewireDiscover::add($this->PREFIX, $this->NAMESPACE, $this->CLASS_PATH);
+
+    $this->artisan('livewire-discover:make TestComponent --prefix='.$this->PREFIX)
+        ->expectsOutputToContain('COMPONENT CREATED');
+
+    LaravelLivewireDiscover::clean();
+});
