9fafad87bd8a3179a909bc28948be47f58a055aa
Jose Erick Carreon <support@joserick.com>
2024-10-20 16:46:09 -0600

    feat: Custom first component created

--- src/Commands/MakeDiscoverCommand.php
+++ src/Commands/MakeDiscoverCommand.php
@@ -2,6 +2,8 @@
 
 namespace Joserick\LaravelLivewireDiscover\Commands;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 use Joserick\LaravelLivewireDiscover\LaravelLivewireDiscover;
 use Livewire\Features\SupportConsoleCommands\Commands\ComponentParser;
 use Livewire\Features\SupportConsoleCommands\Commands\MakeCommand;
@@ -15,29 +17,31 @@ class MakeDiscoverCommand extends MakeCommand
 
     protected $description = 'Create a new Livewire Discover component';
 
-    protected $class_path;
+    protected Collection $class_namespaces;
+
+    protected ?string $class_path;
 
     public function handle()
     {
-        $class_namespaces = LaravelLivewireDiscover::getClassNamespaces();
+        $this->class_namespaces = LaravelLivewireDiscover::getClassNamespaces();
 
-        if ($class_namespaces->isEmpty()) {
+        if ($this->class_namespaces->isEmpty()) {
             $this->error('There are no Livewire-Discover prefixes defined.');
 
             return;
         }
 
-        $prefix = $this->option('prefix') ?? $this->choice('Select the prefix to use', $class_namespaces->keys()->toArray());
+        $prefix = $this->option('prefix') ?? $this->choice('Select the prefix to use', $this->class_namespaces->keys()->toArray());
 
-        if ($class_namespaces[$prefix]['class_path']) {
-            $this->class_path = $class_namespaces[$prefix]['class_path'];
+        if ($this->class_namespaces[$prefix]['class_path']) {
+            $this->class_path = $this->class_namespaces[$prefix]['class_path'];
         } else {
             $this->error("There is no class path defined for the prefix $prefix");
 
             return;
         }
 
-        config(['livewire.class_namespace' => $class_namespaces[$prefix]['class_namespace']]);
+        config(['livewire.class_namespace' => $this->class_namespaces[$prefix]['class_namespace']]);
 
         $this->input->setOption('inline', true);
 
@@ -52,5 +56,29 @@ class MakeDiscoverCommand extends MakeCommand
         $baseClassPath->setValue($this->parser, $this->class_path);
 
         return parent::createClass($force, $inline);
+    }
+
+    public function isFirstTimeMakingAComponent()
+    {
+        foreach ($this->class_namespaces as $data) {
+            if ($data['class_path'] && File::isDirectory($data['class_path'])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function writeWelcomeMessage()
+    {
+        $asciiLogo = <<<EOT
+            <fg=cyan> _     _                    _            ____  _</>
+            <fg=cyan>| |   (_)_   _______      _(_)_ __ ___  |  _ \(_)___  ___ _____   _____ _ __</>
+            <fg=cyan>| |   | \ \ / / _ \ \ /\ / / | '__/ _ \ | | | | / __|/ __/ _ \ \ / / _ \ '__|</>
+            <fg=cyan>| |___| |\ V /  __/\ V  V /| | | |  __/ | |_| | \__ \ (_| (_) \ V /  __/ |</>
+            <fg=cyan>|_____|_| \_/ \___| \_/\_/ |_|_|  \___| |____/|_|___/\___\___/ \_/ \___|_|</>
+            EOT;
+        $this->line("\n".$asciiLogo."\n");
+        $this->line("\n<options=bold>Congratulations, you've created your first Livewire Discover component!</> 🎉🎉🎉\n");
     }
 }
--- src/Commands/MakeLivewireDiscoverCommand.php
+++ src/Commands/MakeLivewireDiscoverCommand.php
@@ -2,6 +2,9 @@
 
 namespace Joserick\LaravelLivewireDiscover\Commands;
 
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'make:livewire-discover')]
 class MakeLivewireDiscoverCommand extends MakeDiscoverCommand
 {
     protected $signature = 'make:livewire-discover {name} {--prefix= : The prefix to use} {--force} {--inline} {--test} {--pest} {--stub= : If you have several stubs, stored in subfolders }';
